9f20a42cfd966df770663ac71881d6f8f55ace57
soutbiaafnts <bia.fontes653@gmail.com>
2026-03-31 14:43:16 -0300

    A fazer

--- Trabalho 01/insert.php
+++ Trabalho 01/insert.php
@@ -4,5 +4,5 @@
     2. conectar com o banco de dados
     3. validar os dados
     4. Se estiver ok, insere no BD
-    5. Se não estiver ok, cancela->volta para a tela inicial->informa os erros
+    5. Se não estiver ok, retornar para a página inicial lá serão exibidas as mensagens de erro e os dados anteriores
 */
