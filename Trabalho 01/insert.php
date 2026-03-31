3b1271fe2cc0c9371beeb78dd84921c649309bed
soutbiaafnts <bia.fontes653@gmail.com>
2026-03-31 14:41:26 -0300

    Início do Trabalho 01

---
+++ Trabalho 01/insert.php
@@ -0,0 +1,8 @@
+<?php
+
+/*  1. pegar as informações do vetor post e guardar em variáveis
+    2. conectar com o banco de dados
+    3. validar os dados
+    4. Se estiver ok, insere no BD
+    5. Se não estiver ok, cancela->volta para a tela inicial->informa os erros
+*/
