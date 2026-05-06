508efd49201e9cb418a3da269197b20c93e04cdc
soutbiaafnts <bia.fontes653@gmail.com>
2026-05-05 23:37:48 -0300

    feat: final do desafio da aula e do relatório

--- Aula 006/blog_pratica/blog/app/Views/quem_somos_content.php
+++ Aula 006/blog_pratica/blog/app/Views/quem_somos_content.php
@@ -27,4 +27,14 @@ echo "<br/>";
 
 echo "Data e Hora: " . date('d/m/Y H:i:s');
 
+echo "<table border='1' cellpadding='5' cellspacing='0'>";
+echo "<tr><th>Aluno</th><th>Nota</th></tr>";
+foreach ($notas as $n) {
+    echo "<tr>";
+    echo "<td>" . $n['aluno'] . "</td>";
+    echo "<td>" . $n['nota'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
 ?>
--- aula-03/connection.php
+++ aula-03/connection.php
@@ -1,5 +1,5 @@
 <?php
-
+git pull
 class Connection {
     private $host;
     private $dbname;
