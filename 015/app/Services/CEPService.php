30d4f3d04b84e2ad3dcc3a5fed5a7d94b905d718
soutbiaafnts <bia.fontes653@gmail.com>
2026-06-16 13:54:18 -0300

    feat(service cep): implement initial service cep

---
+++ 015/app/Services/CEPService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services;
+
+class CEPService {
+    public function consultaCEP (string $cep) {
+        return [
+            "status" => "error",
+            "msg" => "implementar ainda"
+        ];
+    }
+}
